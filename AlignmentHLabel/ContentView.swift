659b38e1996bfaf759ad9d1d8720dc2a43cb6b21
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-04-08 14:29:56 +0300

    init

--- AlignmentHLabel/ContentView.swift
+++ AlignmentHLabel/ContentView.swift
@@ -8,9 +8,40 @@
 
 import SwiftUI
 
+struct HLabel: View {
+    let text: String
+    let backgroundColor: Color
+    let width: CGFloat
+    
+    var body: some View {
+        Text(text)
+            .font(.largeTitle)
+            .frame(width: width)
+            .padding(.vertical)
+            .background(backgroundColor)
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack(alignment: .leading) {
+            HLabel(text: "0", backgroundColor: .gray, width: 100)
+            
+            HLabel(text: "1", backgroundColor: .red, width: 120)
+                .alignmentGuide(.leading, computeValue: { $0[.leading] })
+            
+            HLabel(text: "2", backgroundColor: .yellow, width: 140)
+                .alignmentGuide(.leading, computeValue: { $0[HorizontalAlignment.center] })
+            
+            HLabel(text: "3", backgroundColor: .green, width: 160)
+                .alignmentGuide(.leading, computeValue: { $0[.trailing] })
+            
+            HLabel(text: "4", backgroundColor: .blue, width: 180)
+                .alignmentGuide(.leading, computeValue: { _ in 60 })
+            
+            HLabel(text: "5", backgroundColor: .purple, width: 200)
+                .alignmentGuide(.trailing, computeValue: { _ in 60 })
+        }
     }
 }
 
